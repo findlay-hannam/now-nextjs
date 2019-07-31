5a8aa35f4803266319549b82f6f5d9a9bdbe9f34
Findlay Hannam <f.hannam@elsevier.com>
2019-07-31 11:51:16 +0100

    Get sonnets working functionally

--- components/sonnets.js
+++ components/sonnets.js
@@ -1,5 +1,31 @@
+import React, { Component } from 'react';
 import sonnets from '../resources/sonnets';
 
-export default function Sonnets() {
-  return <div />;
+function Sonnet({
+  'sonnet-name': sonnetName,
+  sonnet,
+}) {
+  return (
+    <div>
+      <h2>{sonnetName}</h2>
+      {sonnet.map(line => <div>{line}</div>)}
+    </div>
+  );
+}
+
+export default class Sonnets extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedSonnet: 0,
+    }
+  }
+  render() {
+    return (
+      <div>
+        {sonnets.map(({ number }) => <button onClick={() => this.setState({ selectedSonnet: number - 1})}>{number}</button>)}
+        <Sonnet {...sonnets[this.state.selectedSonnet]} />
+      </div>
+    );  
+  }
 }
